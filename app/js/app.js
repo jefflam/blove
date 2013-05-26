2cda5a225116e5b271a406ee3bb213231abfddab
Jeff Lam Tian Hung <jefflamth@gmail.com>
2013-05-25 23:44:43 -0700

    Many changes

--- app/js/app.js
+++ app/js/app.js
@@ -1,10 +1,67 @@
-'use strict';
-
-
 // Declare app level module which depends on filters, and services
-angular.module('myApp', ['myApp.filters', 'myApp.services', 'myApp.directives', 'myApp.controllers']).
-  config(['$routeProvider', function($routeProvider) {
-    $routeProvider.when('/view1', {templateUrl: 'partials/partial1.html', controller: 'MyCtrl1'});
-    $routeProvider.when('/view2', {templateUrl: 'partials/partial2.html', controller: 'MyCtrl2'});
-    $routeProvider.otherwise({redirectTo: '/view1'});
-  }]);
+var blove = angular.module('blove', []);
+
+blove.config(['$routeProvider', function($routeProvider) {
+  $routeProvider.when('/', {
+    templateUrl: 'home.html',
+    controller: 'HomeCtrl'
+  })
+  // .when('/:location', {
+  //   templateUrl: 'list.html',
+  //   controller: 'ListCtrl'
+  // })
+  // .otherwise({
+  //   redirectTo: '/view1'
+  // });
+}]);
+
+blove.controller('HomeCtrl', function($scope) {
+
+});
+
+blove.controller('ListCtrl', function($scope) {
+
+  $scope.scrollWindow = function() {
+    jQuery('body').animate({scrollTop: 515}, 500);
+    // Bad code to use jquery, but for animation sake!
+    jQuery('.listing-container').animate({opacity: 1}, 500);
+    jQuery('.baby-cta').animate({opacity: 0}, 500);
+  };
+
+  $scope.babysitters = [
+    {
+      firstName: 'Teresa',
+      lastName: 'Aldrige',
+      description: 'Curabitur blandit tempus porttitor. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum.',
+      portrait: 'http://placehold.it/180x180',
+      location: 'Palo Alto, CA'
+    },
+    {
+      firstName: 'Ana',
+      lastName: 'Navarro',
+      description: 'Curabitur blandit tempus porttitor. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum.',
+      portrait: 'http://placehold.it/180x180',
+      location: 'Stanford, CA'
+    },
+    {
+      firstName: 'Fei',
+      lastName: 'Fei',
+      description: 'Curabitur blandit tempus porttitor. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum.',
+      portrait: 'http://placehold.it/180x180',
+      location: 'Mountain View, CA'
+    },
+    {
+      firstName: 'Melissa',
+      lastName: 'Miranda',
+      description: 'Curabitur blandit tempus porttitor. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum.',
+      portrait: 'http://placehold.it/180x180',
+      location: 'Sunnyvale, CA'
+    }
+  ];
+});
+
+blove.directive('show', function() {
+  return function(scope, element, attrs) {
+    element.addClass('hide');
+  };
+});
