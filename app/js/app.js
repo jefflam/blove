23ee22d3f0f15c9204c50f1fc5d8f938425008d1
Jeff Lam Tian Hung <jefflamth@gmail.com>
2013-05-26 18:35:53 -0700

    more changes and i suck at angular :(

--- app/js/app.js
+++ app/js/app.js
@@ -16,16 +16,18 @@ blove.config(['$routeProvider', function($routeProvider) {
 }]);
 
 blove.controller('HomeCtrl', function($scope) {
-
 });
 
 blove.controller('ListCtrl', function($scope) {
-
   $scope.scrollWindow = function() {
     jQuery('body').animate({scrollTop: 515}, 500);
     // Bad code to use jquery, but for animation sake!
     jQuery('.listing-container').animate({opacity: 1}, 500);
     jQuery('.baby-cta').animate({opacity: 0}, 500);
+  };
+
+  $scope.displayDetailTab = function(tab) {
+    console.log('yo ' + tab);
   };
 
   $scope.babysitters = [
@@ -60,8 +62,118 @@ blove.controller('ListCtrl', function($scope) {
   ];
 });
 
-blove.directive('show', function() {
-  return function(scope, element, attrs) {
-    element.addClass('hide');
+blove.directive('listingDetailButtons', function() {
+  return {
+    restrict: 'E',
+    scope: {},
+    templateUrl: 'listing-detail-buttons.html'
+  };
+});
+
+blove.directive('reviewsBtn', function() {
+  return {
+    scope: true,
+    link: function(scope, element, attrs) {
+      element.click('click', function() {
+        var parentListingView = element.parent().parent().parent().parent();
+        console.log(parentListingView.css('height'));
+        if (parentListingView.css('height') === '180px') {
+          element.parent().parent().parent().parent().css('height', 620);
+          angular.element('.listing-detail-container .reviews').addClass('show');
+        } else {
+          element.parent().parent().parent().parent().css('height', 180);
+          angular.element('.listing-detail-container .reviews').removeClass('show');
+        }
+      });
+    }
+  };
+});
+
+blove.directive('scheduleBtn', function() {
+  return {
+    scope: true,
+    link: function(scope, element, attrs) {
+      element.click('click', function() {
+        var parentListingView = element.parent().parent().parent().parent();
+        console.log(parentListingView.css('height'));
+        if (parentListingView.css('height') === '180px') {
+          element.parent().parent().parent().parent().css('height', 700);
+          angular.element('.listing-detail-container .schedule').addClass('show');
+        } else {
+          element.parent().parent().parent().parent().css('height', 180);
+          angular.element('.listing-detail-container .schedule').removeClass('show');
+        }
+      });
+    }
+  };
+});
+
+blove.directive('videoBtn', function() {
+  return {
+    scope: true,
+    link: function(scope, element, attrs) {
+      element.click('click', function() {
+        var parentListingView = element.parent().parent().parent().parent();
+        console.log(parentListingView.css('height'));
+        if (parentListingView.css('height') === '180px') {
+          element.parent().parent().parent().parent().css('height', 580);
+          angular.element('.listing-detail-container .video').addClass('show');
+        } else {
+          element.parent().parent().parent().parent().css('height', 180);
+          angular.element('.listing-detail-container .video').removeClass('show');
+        }
+      });
+    }
+  };
+});
+
+blove.directive('messageBtn', function() {
+  return {
+    scope: true,
+    link: function(scope, element, attrs) {
+      element.click('click', function() {
+        var parentListingView = element.parent().parent().parent().parent();
+        console.log(parentListingView.css('height'));
+        if (parentListingView.css('height') === '180px') {
+          element.parent().parent().parent().parent().css('height', 500);
+          angular.element('.listing-detail-container .message').addClass('show');
+        } else {
+          element.parent().parent().parent().parent().css('height', 180);
+          angular.element('.listing-detail-container .message').removeClass('show');
+        }
+      });
+    }
+  };
+});
+
+// blove.directive('listingDetailTab', function() {
+//   return {
+//     require: 'ListCtrl',
+//     restrict: 'E',
+//     scope: {},
+//     templateUrl: 'listing-detail-tab.html'
+//   };
+// });
+
+// blove.controller('ListDetailCtrl', function($scope) {
+//   $scope.displayTab = function() {
+//     if (this.on === false) {
+//       this.on = true;
+//     } else {
+//       this.on = false;
+//     }
+//   };
+// });
+
+blove.directive('toggle', function() {
+  return {
+    scope: true,
+    link: function(scope, element, attrs) {
+      scope.on = false;
+
+      scope.toggle = function() {
+        scope.on = !scope.on;
+      };
+    }
   };
 });
